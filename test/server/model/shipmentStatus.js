25e47d068cf766a669d9e76936f7d1516a0bd388
jaseem90 <jaseem@fazaa.ae>
2021-12-05 10:51:31 +0400

    server final

--- test/server/model/shipmentStatus.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const shipmentStatusSchema = mongoose.Schema({
-    shipmentStatus: String,
-})
-
-var shipmentStatusData = mongoose.model("shipmentStatusData", shipmentStatusSchema)
-
-export default shipmentStatusData
--- test/src/components/Navigation/MainNavigation/MainNavigation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MainNavigation = () => {
-    return (
-        <Box></Box>
-    )
-}
-
-export default MainNavigation

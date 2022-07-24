087fd246bbe1d74e8134bc12e05cbb1d77a4f638
drofoxbrandings <85907677+drofoxbrandings@users.noreply.github.com>
2022-07-24 18:17:48 +0400

    Update Authentication.js

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -4,7 +4,8 @@ export const auth = (req, res, next) => {
         const token = req.headers['authorization']
         const auth = token && token.split(' ')[1];
         if (!auth)
-            return res.status(401).send("No authentication token, access denied");
+//             return res.status(401).send("No authentication token, access denied");
+           return res.send(401, { error: "some elaborate error message"});
         const verified = jwt.verify(auth, process.env.JWT_SECRET);
         if (!verified)
             return res.status(401).json({ message: "Token verification failed, authorization denied" });

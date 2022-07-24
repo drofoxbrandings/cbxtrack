6f07f63de90ac805f14ce10d5ef4e44bd4150962
fazaaITRepo <git@fazaa.ae>
2022-07-24 17:50:36 +0400

    response status code set

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -4,13 +4,13 @@ export const auth = (req, res, next) => {
         const token = req.headers['authorization']
         const auth = token && token.split(' ')[1];
         if (!auth)
-            return res.json({ status: '401', message: "No authentication token, access denied" });
+            return res.status(401).json({ message: "No authentication token, access denied" });
         const verified = jwt.verify(auth, process.env.JWT_SECRET);
         if (!verified)
-            return res.json({ status: "401", message: "Token verification failed, authorization denied" });
+            return res.status(401).json({ message: "Token verification failed, authorization denied" });
         req.user = verified.id;
         next();
     } catch (err) {
-        res.json({ status: "500", message: err.message });
+        res.status(500).json({ message: err.message });
     }
 }
